18d8fe38c2b2c97a1e8921a30445af7ca6f8d5c2
Lam Nguyen Phuong <lam.nguyen@eastgate-software.com>
2022-03-22 00:16:40 +0700

    Test New Keypad

---
+++ KeypadModule/ViewModels/OtherKeypadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Demo.Core.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeypadModule.ViewModels
+{
+    public class OtherKeypadViewModel : ViewModelBase
+    {
+    }
+}
